008f2464f43bfc635bb15d6b464f7292ad951e5f
Ianyan27 <gerome5252005@gmail.com>
2025-11-30 19:02:06 +0800

    added sidebar

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,38 +1,77 @@
-let navBahavior = true; // true = expanded, false = collapsed
+    let navBahavior = true;
 
-function expandNavBar() {
-    console.log("expandNavBar called");
     const nav = document.getElementById("navBar");
-    nav.style.width = "225px";
+    const main = document.getElementById("main");
+    const burger = document.getElementById("burger-container");
+    const webName = document.getElementById("web-name");
+    const firstListItem = document.getElementById("first-list-item");
+    const burgerIcon = document.getElementById("burger-icon");
 
     const items = document.querySelectorAll(".nav-list-items");
-    items.forEach(item => {
-    item.style.display = "block"; // or inline-block/flex depending on your design
-    });
+    const navTexts = document.querySelectorAll(".nav-text");
+    const navIcons = document.querySelectorAll(".fa-solid");
 
-    navBahavior = true;
-    console.log("navBahavior:", navBahavior);
-}
+    function expandNavBar() {
+        console.log("expandNavBar called");
 
-function collapseNavBar() {
-    console.log("collapseNavBar called");
-    const nav = document.getElementById("navBar");
-    nav.style.width = "25px";
+        webName.style.display = "block";    
+        burger.style.width = "75px";
+        nav.style.width = "225px";
+        main.style.marginLeft = "225px";
+        main.style.paddingLeft = "20px";
+        firstListItem.style.justifyContent = "space-between";
 
-    const items = document.querySelectorAll(".nav-list-items");
-    items.forEach(item => {
-    item.style.display = "none";
-    });
+        items.forEach(item => {
+        item.style.display = "inline";
+        });
 
-    navBahavior = false;
-    console.log("navBahavior:", navBahavior);
-}
+        navTexts.forEach(item => {
+            item.style.display = "inline";
+        })
 
-function toggleNavBar() {
-    console.log("toggleNavBar called, navBahavior =", navBahavior);
-    if (navBahavior) {
-    collapseNavBar();
-    } else {
-    expandNavBar();
+        navIcons.forEach(item => {
+            item.style.width = "35px";
+        })
+
+        burgerIcon.className = "fa-solid fa-angle-right";
+
+        navBahavior = true;
+        console.log("navBahavior:", navBahavior);
     }
-}
+
+    function collapseNavBar() {
+        console.log("collapseNavBar called");
+
+        webName.style.display = "none";
+        burger.style.width = "20px";
+        nav.style.width = "50px";
+        main.style.marginLeft = "50px";    
+        main.style.paddingLeft = "20px";
+        firstListItem.style.justifyContent = "center";
+
+        items.forEach(item => {
+            item.style.display = "inline";
+        });
+
+        navTexts.forEach(item => {
+            item.style.display = "none";
+        })
+
+        navIcons.forEach(item => {
+            item.style.width = "20px";
+        })
+
+        burgerIcon.className = "fa-solid fa-bars";
+
+        navBahavior = false;
+        console.log("navBahavior:", navBahavior);
+    }
+
+    function toggleNavBar() {
+        console.log("toggleNavBar called, navBahavior =", navBahavior);
+        if (navBahavior) {
+        collapseNavBar();
+        } else {
+        expandNavBar();
+        }
+    }
